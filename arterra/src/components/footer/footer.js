d4dccb999597f936165a18301cb18e950aa34a7b
dillomn <dillonmbrooks@gmail.com>
2023-04-24 23:40:20 +1000

    Created: footer.js

---
+++ arterra/src/components/footer/footer.js
@@ -0,0 +1,37 @@
+import "./footer.css"; // import the CSS file
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare, faInstagramSquare, faLinkedin, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons'
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <table>
+        <tr>
+          <td>
+            <a href="https://www.linkedin.com/company/arterra-interactive" target="_blank" rel="noopener noreferrer" className="nav-link">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </td>
+          <td>
+            <a href="https://www.youtube.com/channel/UCKqV5EMjgpiHoBc4sLszHgQ" target="_blank" rel="noopener noreferrer" className="nav-link">
+              <FontAwesomeIcon icon={faYoutubeSquare} />
+            </a>
+          </td>
+          <td>
+            <a href="https://www.instagram.com/arterrainteractive" target="_blank" rel="noopener noreferrer" className="nav-link">
+              <FontAwesomeIcon icon={faInstagramSquare} />
+            </a>
+          </td>
+          <td>
+            <a href="https://www.facebook.com/ArterraInteractive/" target="_blank" rel="noopener noreferrer" className="nav-link">
+              <FontAwesomeIcon icon={faFacebookSquare} />
+            </a>
+          </td>
+        </tr>
+      </table>
+      <p>© 2023 Arterra Interactive.</p>
+    </div>
+  );
+}
+
+export default Footer;
