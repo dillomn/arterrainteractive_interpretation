e9d4009304c18fa370408f5730731917499cd702
dillomn <dillonmbrooks@gmail.com>
2023-04-26 21:08:28 +1000

    Modified: Added Blur Effect When Scrolling

--- src/components/landinganimation/landinganimation.js
+++ src/components/landinganimation/landinganimation.js
@@ -1,14 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import Typewriter from "typewriter-effect";
 import "./landinganimation.css"; // import the CSS file
 import bg from "./bg.mp4"
+
 
 function LandingAnimation() {
   const words = ["3D Planning", "Post Production", "3D Visualisation", "Interactive Media", "Constuction", "CGI", "Animation"]; // list of words
   const delay = 100; // time delay in milliseconds
 
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      setIsScrolled(scrollTop > 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div className="image-container">
-      <video className='bgvideo' autoPlay loop muted>
+      <video className={`bgvideo ${isScrolled ? 'scrolled' : ''}`} autoPlay loop muted>
           <source src={bg} type='video/mp4' />
       </video>
         <div className="typewriter">
