4e3239ab4d6dee2d19645c3dffcaf5cf2e725923
dillomn <dillonmbrooks@gmail.com>
2023-05-01 18:20:18 +1000

    Created: Added a 404 page

---
+++ src/components/404/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './404.css';
+
+function Page404() {
+  return (
+    <div class="err">
+        <h1>Something went wrong ;(</h1>
+        <p>Oops! Looks like the page you are trying to reach doesn't exist...</p>
+    </div>
+  );
+}
+
+export default Page404;
