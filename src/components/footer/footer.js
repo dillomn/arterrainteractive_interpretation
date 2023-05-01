d3e720540f0637ebd0a7f8e7e147b59557cf6457
dillomn <dillonmbrooks@gmail.com>
2023-05-01 19:25:19 +1000

    Modified: Swapped from using table to list

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,30 +5,24 @@ import { faFacebookSquare, faInstagramSquare, faLinkedin, faYoutubeSquare } from
 function Footer() {
   return (
     <div className="footer-container">
-      <table>
-        <tr>
-          <td>
+      <ul className="nav-link-footer">
             <a href="https://www.linkedin.com/company/arterra-interactive" target="_blank" rel="noopener noreferrer" className="nav-link-footer">
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
-          </td>
-          <td>
+
             <a href="https://www.youtube.com/channel/UCKqV5EMjgpiHoBc4sLszHgQ" target="_blank" rel="noopener noreferrer" className="nav-link-footer">
               <FontAwesomeIcon icon={faYoutubeSquare} />
             </a>
-          </td>
-          <td>
+
             <a href="https://www.instagram.com/arterrainteractive" target="_blank" rel="noopener noreferrer" className="nav-link-footer">
               <FontAwesomeIcon icon={faInstagramSquare} />
             </a>
-          </td>
-          <td>
+
             <a href="https://www.facebook.com/ArterraInteractive/" target="_blank" rel="noopener noreferrer" className="nav-link-footer">
               <FontAwesomeIcon icon={faFacebookSquare} />
             </a>
-          </td>
-        </tr>
-      </table>
+
+      </ul>
       <p>© 2023 Arterra Interactive.</p>
     </div>
   );
